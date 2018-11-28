17e0c98b2bf0cdd9744e8ac4caff2702523afe19
iDonJose <donor.develop@gmail.com>
2018-11-28 15:20:48 +0100

    Added UIImage(gradient:shape:size:) & UIImage(gradient:shapeForBounds:size:)

--- Sources/UIKit/UIImage.swift
+++ Sources/UIKit/UIImage.swift
@@ -8,6 +8,8 @@
 
 extension UIImage {
 
+	// MARK: - Image from Color
+
 	/// Creates an Image of a given shape and color
 	public convenience init(color: UIColor,
 							shape: UIBezierPath?,
@@ -41,6 +43,54 @@ extension UIImage {
 
 		color.setFill()
 		path.fill()
+
+		let image = UIGraphicsGetCurrentContext()?.makeImage()
+
+		UIGraphicsEndImageContext()
+
+		if let image = image { self.init(cgImage: image) }
+		else { self.init() }
+
+	}
+
+
+	// MARK: - Image from Gradient
+
+	/// Creates an Image of a given shape and gradient
+	public convenience init(gradient: Gradient,
+							shape: UIBezierPath?,
+							size: CGSize) {
+		self.init(gradient: gradient,
+				  shapeForBounds: { shape ?? UIBezierPath(rect: $0) },
+				  size: size)
+	}
+
+	#if USE_SVG
+	/// Creates an Image of a given shape and gradient
+	public convenience init(gradient: Gradient,
+							shape: BezierPath,
+							size: CGSize) {
+		self.init(gradient: gradient,
+				  shapeForBounds: shape.pathForBounds,
+				  size: size)
+	}
+	#endif
+
+	/// Creates an Image of a given shape and gradient
+	public convenience init(gradient: Gradient,
+							shapeForBounds: ((CGRect) -> UIBezierPath)?,
+							size: CGSize) {
+
+		let isOpaque = shapeForBounds == nil
+		let bounds = CGRect(origin: .zero, size: size)
+		let path = shapeForBounds?(bounds) ?? UIBezierPath(rect: bounds)
+
+		UIGraphicsBeginImageContextWithOptions(size, isOpaque, 0)
+		let context = UIGraphicsGetCurrentContext()!
+
+		context.addPath(path.cgPath)
+		context.clip()
+		gradient.draw(in: bounds, context: context)
 
 		let image = UIGraphicsGetCurrentContext()?.makeImage()
 
