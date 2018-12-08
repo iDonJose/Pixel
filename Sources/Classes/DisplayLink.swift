f603cfcc50fa30a93542cf69c0d25f7e9fd1c68e
iDonJose <donor.develop@gmail.com>
2018-12-08 14:09:59 +0100

    Added DisplayLink

---
+++ Sources/Classes/DisplayLink.swift
@@ -0,0 +1,87 @@
+//
+//  DisplayLink.swift
+//  Pixel-iOS
+//
+//  Created by José Donor on 06/12/2018.
+//
+
+
+public final class DisplayLink: NSObject {
+
+	/// Display link
+	private lazy var displayLink: CADisplayLink? = CADisplayLink(target: self, selector: #selector(didUpdate(displaylink:)))
+
+	/// Update block
+    private var update: ((_ timestamp: Double, _ isCompleted: Bool) -> Void)?
+
+	/// Lifespan
+	private let lifespan: Double?
+    /// Start timestamp
+    private var start: Double!
+
+
+
+	// MARK: - Initialize
+
+	public init(lifespan: Double? = nil,
+				fps: Int = 0,
+				update: @escaping (_ timestamp: Double, _ isCompleted: Bool) -> Void) {
+
+		self.lifespan = lifespan
+		self.update = update
+
+		super.init()
+
+		if #available(iOS 10.0, *) {
+			displayLink!.preferredFramesPerSecond = fps
+		}
+
+		displayLink!.add(to: .current, forMode: RunLoop.Mode.common)
+
+	}
+
+	deinit {
+		invalidate()
+	}
+
+
+
+	// MARK: - Clean up
+
+	public func invalidate() {
+        update = nil
+		displayLink?.invalidate()
+		displayLink = nil
+	}
+
+
+
+	// MARK: - Call back
+
+	@objc
+	func didUpdate(displaylink: CADisplayLink) {
+
+        if start == nil { start = displaylink.timestamp }
+
+
+        let timestamp = displaylink.timestamp - start
+
+        var isCompleted = false
+
+        if let lifespan = self.lifespan {
+            if #available(iOS 10.0, *) {
+                if displaylink.targetTimestamp - start > lifespan { isCompleted = true }
+            }
+            else {
+                if timestamp >= lifespan { isCompleted = true }
+            }
+        }
+
+
+		update?(timestamp, isCompleted)
+
+        if isCompleted { invalidate() }
+
+	}
+
+}
